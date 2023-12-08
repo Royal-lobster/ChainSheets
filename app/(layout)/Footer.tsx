acdfeec7cb704e03f9d3df2547cf000ec4f55ea7
Yadheedhya <yadheedhya6@gmail.com>
2023-12-09 05:02:19 +0530

    update footer

--- app/(layout)/Footer.tsx
+++ app/(layout)/Footer.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="p-10 text-neutral-400 text-center">
-      🛠️ By BrainDAO Team - A ETH India 2023 Project
+      🛠️ Team BrainDAO - ETHIndia 2023 Project
     </footer>
   );
 };
