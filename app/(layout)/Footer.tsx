cfe426de4ef391739ac49bd60631d0510360d847
Srujan Gurram <srujangs8@gmail.com>
2023-12-09 04:34:07 +0530

    added footer markup

--- app/(layout)/Footer.tsx
+++ app/(layout)/Footer.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Footer = () => {
-	return <div>Footer</div>;
+  return (
+    <footer className="p-10 text-neutral-400 text-center">
+      🛠️ By BrainDAO Team - A ETH India 2023 Project
+    </footer>
+  );
 };
 
 export default Footer;
