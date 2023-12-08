e626d8684e223a9385c9370bc4c6bb7783531391
Yadheedhya <yadheedhya6@gmail.com>
2023-12-09 05:16:47 +0530

    fix invalid DOM property

--- app/(layout)/ChainSheetsIcon.tsx
+++ app/(layout)/ChainSheetsIcon.tsx
@@ -19,9 +19,9 @@ const ChainSheetsIcon = ({ ...props }: SVGProps<SVGSVGElement>) => {
         rx="128"
         fill="url(#r5)"
         stroke="#FFFFFF"
-        stroke-width="0"
-        stroke-opacity="100%"
-        paint-order="stroke"
+        strokeWidth="0"
+        strokeOpacity="100%"
+        paintOrder="stroke"
       ></rect>
       <clipPath id="clip">
         <use xlinkHref="#r4"></use>
@@ -33,8 +33,8 @@ const ChainSheetsIcon = ({ ...props }: SVGProps<SVGSVGElement>) => {
           gradientTransform="rotate(180)"
           style={{ transformOrigin: "center center" }}
         >
-          <stop stop-color="#5C5C5C"></stop>
-          <stop offset="1" stop-color="#0F1015"></stop>
+          <stop stopColor="#5C5C5C"></stop>
+          <stop offset="1" stopColor="#0F1015"></stop>
         </linearGradient>
         <radialGradient
           id="r6"
@@ -44,8 +44,8 @@ const ChainSheetsIcon = ({ ...props }: SVGProps<SVGSVGElement>) => {
           gradientUnits="userSpaceOnUse"
           gradientTransform="translate(256) rotate(90) scale(512)"
         >
-          <stop stop-color="white"></stop>
-          <stop offset="1" stop-color="white" stop-opacity="0"></stop>
+          <stop stopColor="white"></stop>
+          <stop offset="1" stopColor="white" stopOpacity="0"></stop>
         </radialGradient>
       </defs>
       <svg
@@ -56,15 +56,15 @@ const ChainSheetsIcon = ({ ...props }: SVGProps<SVGSVGElement>) => {
         height="352"
         x="80"
         y="80"
-        alignment-baseline="middle"
+        alignmentBaseline="middle"
         style={{ color: "rgb(255, 255, 255)" }}
       >
         <path
           d="m10 5.754-3.984 3.68a1.536 1.536 0 0 0 0 2.294 1.86 1.86 0 0 0 2.482 0L13.25 7.25c1.371-1.267 1.343-3.283-.028-4.55s-3.594-1.267-4.965 0L3.292 7.287c-2.056 1.9-2.056 4.981 0 6.881l.089.082"
           stroke="currentColor"
-          stroke-width="1.5"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="1.5"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         ></path>
       </svg>
     </svg>
