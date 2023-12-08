14d4afeddea9465fd2ec4d98b70c01910f0e3a70
Yadheedhya <yadheedhya6@gmail.com>
2023-12-09 05:12:43 +0530

    add nextjs links

--- app/(layout)/Navbar.tsx
+++ app/(layout)/Navbar.tsx
@@ -1,20 +1,27 @@
 import React from "react";
 import ChainSheetsIcon from "./ChainSheetsIcon";
 import { IconCirclePlus } from "@tabler/icons-react";
+import Link from "next/link";
 
 const Navbar = () => {
   return (
     <header className="flex justify-center md:justify-between gap-4 items-center py-4 flex-wrap">
       <div className="flex gap-2 items-center">
-        <ChainSheetsIcon className="w-8 h-8" />
-        <span className="text-2xl font-bold">ChainSheet</span>
+        <Link href="/">
+          <span className="cursor-pointer flex gap-2 items-center">
+            <ChainSheetsIcon className="w-8 h-8" />
+            <span className="text-2xl font-bold">ChainSheet</span>
+          </span>
+        </Link>
       </div>
       <div className="flex gap-8">
         <nav className="flex gap-4 items-center">
-          <a href="/#create-journal" className="text-neutral-500 flex gap-2">
-            <IconCirclePlus />
-            <span>Create new Journal</span>
-          </a>
+          <Link href="/path-for-create-new-journal">
+            <span className="cursor-pointer text-neutral-500 flex gap-2">
+              <IconCirclePlus />
+              <span>Create new Journal</span>
+            </span>
+          </Link>
         </nav>
         <button className="bg-black text-white px-4 py-2 rounded-lg">
           Connect Wallet
