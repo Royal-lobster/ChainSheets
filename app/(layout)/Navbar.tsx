34e7b0cca88a33033b01b12bde2e73e66fcd8ea9
Srujan Gurram <srujangs8@gmail.com>
2023-12-09 04:07:38 +0530

    Create ChainSheetsIcon component and update Navbar component to include the ChainSheetsIcon. Also update package.json dependencies and devDependencies

--- app/(layout)/Navbar.tsx
+++ app/(layout)/Navbar.tsx
@@ -1,7 +1,15 @@
 import React from "react";
+import ChainSheetsIcon from "./ChainSheetsIcon";
 
 const Navbar = () => {
-	return <div>Navbar</div>;
+  return (
+    <div>
+      <div className="flex gap-1 items-center">
+        <ChainSheetsIcon className="w-8 h-8" />
+        <span className="text-2xl font-bold">ChainSheet</span>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
