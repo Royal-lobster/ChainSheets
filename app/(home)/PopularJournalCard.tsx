33c708bd43313ee8e8acb5f5f92764836ffa497a
Srujan Gurram <srujangs8@gmail.com>
2023-12-09 08:23:20 +0530

    extracted dummy data to default props in other components as well

--- app/(home)/PopularJournalCard.tsx
+++ app/(home)/PopularJournalCard.tsx
@@ -1,18 +1,28 @@
 import React from "react";
 
-const PopularJournalCard = () => {
+interface PopularJournalCardProps {
+  title?: string;
+  description?: string;
+  members?: number;
+}
+
+const PopularJournalCard = ({
+  title = "AI and Machine Learning",
+  description = "All papers on AI and Machine learning Research",
+  members = 200,
+}: PopularJournalCardProps) => {
   return (
     <div className="w-full flex flex-shrink-0 bg-white rounded-lg border border-gray-200 shadow-md overflow-hidden">
       <div className="w-24 h-24 m-2 rounded-md bg-purple-600 flex-shrink-0 overflow-hidden"></div>
       <div className="p-4">
         <h1 className="text-lg md:text-xl font-semibold text-gray-900 line-clamp-1">
-          AI and Machine Learning
+          {title}
         </h1>
         <p className="text-gray-700 text-sm md:text-base line-clamp-1">
-          All papers on AI and Machine learning Research
+          {description}
         </p>
         <p className="text-gray-600 bg-red-100 inline-block text-xs md:text-sm mt-2">
-          200 Members
+          {members} Members
         </p>
       </div>
     </div>
--- app/(home)/PopularPapersCard.tsx
+++ app/(home)/PopularPapersCard.tsx
@@ -1,25 +1,37 @@
 import React from "react";
 
-const PopularPapersCard = () => {
+interface PopularPapersCardProps {
+  category?: string;
+  title?: string;
+  description?: string;
+  author?: string;
+  holders?: number;
+}
+
+const PopularPapersCard = ({
+  category = "AI and Machine Learning",
+  title = "Blockchain Vulnerability detection with LLMs",
+  description = "All papers on AI and Machine learning Research",
+  author = "0x145...2352, srujangurram.eth",
+  holders = 200,
+}: PopularPapersCardProps) => {
   return (
     <div className="w-full rounded overflow-hidden shadow-lg p-4 text-gray-700 border border-gray-200">
       <div className="inline px-2 py-1 bg-blue-200 text-blue-800 text-xs font-semibold rounded-full uppercase tracking-wide">
-        AI and Machine Learning
+        {category}
       </div>
       <div className="mt-2 mb-4">
         <div className="text-lg md:text-xl font-bold text-gray-900 line-clamp-2">
-          Blockchain Vulnerability detection with LLMs
+          {title}
         </div>
         <p className="text-gray-700 text-sm md:text-base line-clamp-2">
-          All papers on AI and Machine learning Research
+          {description}
         </p>
       </div>
       <div className="flex justify-between items-center">
-        <span className="text-sm text-gray-600">
-          By 0x145...2352, srujangurram.eth
-        </span>
+        <span className="text-sm text-gray-600">{author}</span>
         <span className="inline-block text-xs px-2 rounded-full uppercase font-semibold tracking-wide">
-          200 Holders
+          {holders} Holders
         </span>
       </div>
     </div>
