bcd4e4a1aadea44a8f405d1247504f0d18f78b70
Srujan Gurram <srujangs8@gmail.com>
2023-12-09 08:55:52 +0530

    Refactor UnderReviewPaperCard to accept props and display dynamic content

--- app/journal/[id]/UnderReviewPaperCard.tsx
+++ app/journal/[id]/UnderReviewPaperCard.tsx
@@ -1,23 +1,33 @@
 import React from "react";
 
-const UnderReviewPaperCard = () => {
+interface UnderReviewPaperCardProps {
+  title?: string;
+  description?: string;
+  reviewCount?: number;
+  createdOn?: string;
+  lastReview?: string;
+}
+
+const UnderReviewPaperCard = ({
+  title = "Blockchain Vulnerability detection with LLMs",
+  description = "All papers on AI and Machine learning Research",
+  reviewCount = 10,
+  createdOn = "12th Oct 2023",
+  lastReview = "4th Dec 2023",
+}: UnderReviewPaperCardProps) => {
   return (
     <div className="w-full rounded overflow-hidden shadow-md border border-neutral-200 p-4 bg-white flex flex-col md:flex-row justify-between items-center">
       {/* Left side */}
       <div className="md:mr-4">
-        <div className="font-bold text-xl mb-2">
-          Blockchain Vulnerability detection with LLMs
-        </div>
-        <p className="text-neutral-700 text-base">
-          All papers on AI and Machine learning Research
-        </p>
+        <div className="font-bold text-xl mb-2">{title}</div>
+        <p className="text-neutral-700 text-base">{description}</p>
       </div>
       {/* Center and Right side */}
       <div className="flex flex-grow mt-6 md:items-center w-full md:w-[unset] justify-between text-neutral-600 text-sm md:mt-0 md:ml-4">
         <div className="md:mx-auto">
-          <p>has 10 Reviews</p>
-          <p>Created on 12th Oct 2023</p>
-          <p>Last Review 4th Dec 2023</p>
+          <p>has {reviewCount} Reviews</p>
+          <p>Created on {createdOn}</p>
+          <p>Last Review {lastReview}</p>
         </div>
         <button className="bg-black text-white py-2 px-4 rounded hover:bg-neutral-800 transition-colors duration-300 mt-4 md:mt-0">
           Review
