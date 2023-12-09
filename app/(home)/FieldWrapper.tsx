3173d0e0f5b5ec1291d2f07ab35287e9b226216f
Yadheedhya <yadheedhya6@gmail.com>
2023-12-09 14:22:12 +0530

    add slidebar range

--- app/(home)/FieldWrapper.tsx
+++ app/(home)/FieldWrapper.tsx
@@ -1,3 +1,4 @@
+import React, { useState } from "react";
 import { UseFormRegister, FieldErrors } from "react-hook-form";
 
 type FieldWrapperProps = {
@@ -19,16 +20,30 @@ const FieldWrapper = ({
   type = "text",
   children,
 }: FieldWrapperProps) => {
+  // State to hold the value of the range input
+  const [rangeValue, setRangeValue] = useState("");
+
+  // Function to handle range value change
+  const handleRangeChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setRangeValue(event.target.value);
+  };
+
   return (
     <div className="my-4">
       <label className="block text-lg mb-2 text-neutral-400">{label}:</label>
-      {children ? (
-        children
-      ) : (
+      {type === "range" ? (
+        <div className="range-container">
+          <input
+            {...register(name)}
+            type={type}
+            className="block border w-full p-2 rounded text-black"
+            onChange={handleRangeChange}
+          />
+          <div className="range-value">{rangeValue}%</div>
+        </div>
+      ) : children || (
         <input
-          {...register(name, {
-            valueAsNumber: type === "number" || type === "range",
-          })}
+          {...register(name)}
           placeholder={placeholder}
           type={type}
           className="block border w-full p-2 rounded text-black"
