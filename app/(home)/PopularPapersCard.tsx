561af82594ac0b0e997d6df7b74834698cebb5f9
Srujan Gurram <srujangs8@gmail.com>
2023-12-09 05:38:17 +0530

    Refactor PopularPapersCard component to improve readability and update content information

--- app/(home)/PopularPapersCard.tsx
+++ app/(home)/PopularPapersCard.tsx
@@ -2,18 +2,25 @@ import React from "react";
 
 const PopularPapersCard = () => {
   return (
-    <div className="w-full flex flex-shrink-0 bg-white rounded-lg border border-gray-200 shadow-md overflow-hidden">
-      <div className="w-24 h-24 m-2 rounded-md bg-purple-600 flex-shrink-0 overflow-hidden"></div>
-      <div className="p-4">
-        <h1 className="text-lg md:text-xl font-semibold text-gray-900 line-clamp-1">
-          AI and Machine Learning
-        </h1>
-        <p className="text-gray-700 text-sm md:text-base line-clamp-1">
+    <div className="w-full rounded overflow-hidden shadow-lg p-4 text-gray-700">
+      <div className="inline px-2 py-1 bg-blue-200 text-blue-800 text-xs font-semibold rounded-full uppercase tracking-wide">
+        AI and Machine Learning
+      </div>
+      <div className="mt-2 mb-4">
+        <div className="text-xl font-bold text-gray-900 line-clamp-2">
+          Blockchain Vulnerability detection with LLMs
+        </div>
+        <p className="text-gray-700 text-base line-clamp-2">
           All papers on AI and Machine learning Research
         </p>
-        <p className="text-gray-600 bg-red-100 inline-block text-xs md:text-sm mt-2">
-          200 Members
-        </p>
+      </div>
+      <div className="flex justify-between items-center">
+        <span className="text-sm text-gray-600">
+          By 0x145...2352, srujangurram.eth
+        </span>
+        <span className="inline-block bg-blue-100 text-blue-800 text-xs px-2 rounded-full uppercase font-semibold tracking-wide">
+          200 Holders
+        </span>
       </div>
     </div>
   );
