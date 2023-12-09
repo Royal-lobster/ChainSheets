5cbe444fc9683a33699b2f40527be2a4af132d36
Srujan Gurram <srujangs8@gmail.com>
2023-12-09 20:25:39 +0530

    Refactor Modal component to include a custom close button with modified styles

--- app/components/Modal.tsx
+++ app/components/Modal.tsx
@@ -33,7 +33,9 @@ const Modal = ({
         <Dialog.Content className="fixed w-[90vw] lg:w-[600px] top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-20 flex flex-col bg-neutral-100 border border-neutral-200 rounded-lg shadow-lg p-6 items-center justify-center">
           <div className="flex justify-between w-full items-center mb-4">
             <h2 className="text-xl font-bold">{title}</h2>
-            <Dialog.Close className={button({ type: "link" })}>
+            <Dialog.Close
+              className={button({ type: "link", class: "m-0 p-0" })}
+            >
               <IconX size={20} />
             </Dialog.Close>
           </div>
