bf5984b125e0af84d4d02b821a5909255ce684b6
Srujan Gurram <srujangs8@gmail.com>
2023-12-09 08:14:18 +0530

    Refactor JournalHeader component to add a gap between flex items and adjust font sizes and margins for better readability and alignment

--- app/journal/[id]/JournalHeader.tsx
+++ app/journal/[id]/JournalHeader.tsx
@@ -4,9 +4,9 @@ import Image from "next/image";
 const JournalHeader = () => {
   return (
     <div className="flex flex-col md:flex-row justify-center sm:justify-between items-center">
-      <div className="flex justify-center sm:justify-between items-center flex-wrap">
+      <div className="flex gap-8 justify-center sm:justify-between items-center flex-wrap">
         {/* Left side image / icon */}
-        <div className="p-4 flex-shrink-0">
+        <div className="flex-shrink-0">
           <Image
             className="h-48 w-48 rounded"
             src="https://source.unsplash.com/random/500x500"
@@ -17,11 +17,11 @@ const JournalHeader = () => {
         </div>
 
         {/* Center content */}
-        <div className="p-4 text-center sm:text-left">
-          <div className="inline-block px-1.5 p-0.5 text-sm rounded-full bg-neutral-200 text-neutral-500">
+        <div className="flex gap-1 flex-col text-center sm:text-left">
+          <div className="w-max px-1.5 p-0.5 text-sm rounded-full bg-neutral-200 text-neutral-500">
             Artificial Intelligence
           </div>
-          <div className="font-bold text-xl md:text-3xl mt-2">
+          <div className="font-bold text-2xl md:text-3xl mt-2">
             AI and Machine Learning
           </div>
           <p className="text-gray-600 text-sm mt-1">
@@ -39,24 +39,24 @@ const JournalHeader = () => {
       </div>
 
       {/* Right side stats */}
-      <div className="p-4 flex flex-col items-center sm:items-start">
+      <div className="flex flex-col mt-10 sm:mt-0 items-center sm:items-start">
         <table className="text-xl">
           <tbody>
             <tr>
               <td className="text-right pr-3">
-                <span className="text-3xl font-bold">1000</span>
+                <span className="text-2xl sm:text-3xl font-bold">1000</span>
               </td>
               <td>Members</td>
             </tr>
             <tr>
               <td className="text-right pr-3">
-                <span className="text-3xl font-bold">120</span>
+                <span className="text-2xl sm:text-3xl font-bold">120</span>
               </td>
               <td>Published Papers</td>
             </tr>
             <tr>
               <td className="text-right pr-3">
-                <span className="text-3xl font-bold">1.4k</span>
+                <span className="text-2xl sm:text-3xl font-bold">1.4k</span>
               </td>
               <td>Holders</td>
             </tr>
