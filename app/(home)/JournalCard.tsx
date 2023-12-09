c106f14721e9e2f5a52c20164d26a9d0c9de2d56
Srujan Gurram <srujangs8@gmail.com>
2023-12-09 08:58:22 +0530

    Add an Image component to JournalCard to display a dummy image with specified size and alt text

--- app/(home)/JournalCard.tsx
+++ app/(home)/JournalCard.tsx
@@ -1,10 +1,13 @@
 import React from "react";
+import Image from "next/image";
 
 interface PopularJournalCardProps {
   title?: string;
   description?: string;
   members?: number;
 }
+
+const JOURNAL_IMAGE_SIZE = 100;
 
 const JournalCard = ({
   title = "AI and Machine Learning",
@@ -13,7 +16,13 @@ const JournalCard = ({
 }: PopularJournalCardProps) => {
   return (
     <div className="w-full flex flex-shrink-0 bg-white rounded-lg border border-neutral-200 shadow-md overflow-hidden">
-      <div className="w-24 h-24 m-2 rounded-md bg-purple-600 flex-shrink-0 overflow-hidden"></div>
+      <Image
+        src="https://source.unsplash.com/random/200x200"
+        alt="Dummy Image"
+        className="w-24 h-24 m-2 rounded-md flex-shrink-0 overflow-hidden"
+        width={JOURNAL_IMAGE_SIZE}
+        height={JOURNAL_IMAGE_SIZE}
+      />
       <div className="p-4">
         <h1 className="text-lg md:text-xl font-semibold text-neutral-900 line-clamp-1">
           {title}
