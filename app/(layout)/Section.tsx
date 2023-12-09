bd9a23e48abc13ded6f49ba2e616a881723666e3
Srujan Gurram <srujangs8@gmail.com>
2023-12-09 07:22:34 +0530

    Refactor CreateNewJournal, Navbar, and Section components to improve responsiveness and alignment

--- app/(layout)/Section.tsx
+++ app/(layout)/Section.tsx
@@ -8,7 +8,7 @@ type SectionProps = {
 const Section = ({ title, children, ...props }: SectionProps) => {
   return (
     <section className="flex flex-col gap-8">
-      <h2 className="text-1xl md:text-2xl font-bold">{title}</h2>
+      <h2 className="text-center sm:text-left text-2xl font-bold">{title}</h2>
       {children}
     </section>
   );
