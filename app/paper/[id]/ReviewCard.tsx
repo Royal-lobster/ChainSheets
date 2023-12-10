e0218282af7e21a9f3c694bd2ef4afa6b4d2d4f2
Srujan Gurram <srujangs8@gmail.com>
2023-12-10 06:18:01 +0530

    Refactored ReviewCard component to improve readability and spacing of author and createdAt information

--- app/paper/[id]/ReviewCard.tsx
+++ app/paper/[id]/ReviewCard.tsx
@@ -21,13 +21,15 @@ const ReviewCard = ({
         <h1 className="text-2xl font-bold">{title}</h1>
         <p className="text-lg">{description}</p>
       </div>
-      <div className="flex gap-2 text-neutral-500">
-        <IconUser />
-        {shortenAddress(author)}
-      </div>
-      <div className="flex gap-2 text-neutral-500">
-        <IconCalendar />
-        {createdAt}
+      <div className="flex gap-4">
+        <div className="flex gap-2 text-neutral-500">
+          <IconUser />
+          {shortenAddress(author)}
+        </div>
+        <div className="flex gap-2 text-neutral-500">
+          <IconCalendar />
+          {createdAt}
+        </div>
       </div>
     </div>
   );
