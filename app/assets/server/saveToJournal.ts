8b6eef2384ec37b545f51c7c021f4ee225214ebe
Yadheedhya <yadheedhya6@gmail.com>
2023-12-10 05:53:37 +0530

    create journal entry

---
+++ app/assets/server/saveToJournal.ts
@@ -0,0 +1,29 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+type CreateJournalArgs = {
+  title: string;
+  description: string;
+  topic: string;
+  daoAddress: string;
+  ipfsImage: string;
+};
+
+export async function createJournal(args: CreateJournalArgs): Promise<void> {
+  try {
+    await prisma.journal.create({
+      data: {
+        title: args.title,
+        description: args.description,
+        topic: args.topic,
+        daoAddress: args.daoAddress,
+        ipfsImage: args.ipfsImage,
+      },
+    });
+    console.log('Journal entry created successfully');
+  } catch (error) {
+    console.error('Error creating journal entry:', error);
+    throw error;
+  }
+}
