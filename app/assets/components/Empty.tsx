fb1915d02e14300e3c0327516c881c82dd6eb775
Srujan Gurram <srujangs8@gmail.com>
2023-12-10 06:55:28 +0530

    Refactor Empty component to accept title and description as props, and create new ErrorPage and NotFound components

--- app/assets/components/Empty.tsx
+++ app/assets/components/Empty.tsx
@@ -1,14 +1,17 @@
 import { IconNorthStar } from "@tabler/icons-react";
 import React from "react";
 
-const Empty = () => {
+const Empty = ({
+  title = "You are too early here",
+  description = "Lets start creating some exciting stuff here 🚀",
+}) => {
   return (
-    <div className="flex flex-col items-center justify-center h-full">
-      <div className="flex items-center space-x-2">
+    <div className="flex flex-col items-center justify-center w-full h-full">
+      <div className="flex text-2xl items-center space-x-2">
         <IconNorthStar className="w-6 h-6" />
-        <div>You are too early here!</div>
+        <div>{title}</div>
       </div>
-      <div>Lets start creating some exciting stuff here 🚀</div>
+      <div className="text-lg text-neutral-500">{description}</div>
     </div>
   );
 };
---
+++ app/error.js
@@ -0,0 +1,9 @@
+"use client"
+import React from 'react'
+import Empty from "./assets/components/Empty";
+
+const ErrorPage = () => {
+  return <Empty title='Oops! The app had a hiccup.' description='Please try to refresh the page and try again.' />
+}
+
+export default ErrorPage
