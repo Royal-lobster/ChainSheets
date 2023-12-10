c9be11665a3be90279da9ffe6fbffdee9e5a0756
Srujan Gurram <srujangs8@gmail.com>
2023-12-10 08:25:40 +0530

    Add Empty component to render when popularJournals and popularPapers arrays are empty

--- app/(home)/PopularJournals.tsx
+++ app/(home)/PopularJournals.tsx
@@ -3,6 +3,7 @@ import Section from "../(layout)/Section";
 import JournalCard from "./JournalCard";
 import { IconBook } from "@tabler/icons-react";
 import prisma from "../assets/server/db";
+import Empty from "../assets/components/Empty";
 
 const PopularJournals = async () => {
   const popularJournals = await prisma.journal.findMany({
@@ -25,6 +26,7 @@ const PopularJournals = async () => {
   return (
     <Section title="Popular Journals" icon={<IconBook size={32} />}>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+        {popularJournals.length === 0 && <Empty />}
         {popularJournals.map((journal) => (
           <JournalCard
             title={journal.title}
--- app/(home)/PopularPapers.tsx
+++ app/(home)/PopularPapers.tsx
@@ -3,6 +3,7 @@ import Section from "../(layout)/Section";
 import PapersCard from "./PapersCard";
 import { IconFiles } from "@tabler/icons-react";
 import prisma from "../assets/server/db";
+import Empty from "../assets/components/Empty";
 
 const PopularPapers = async () => {
   const popularPapers = await prisma.paper.findMany({
@@ -28,6 +29,7 @@ const PopularPapers = async () => {
   return (
     <Section title="Popular Papers" icon={<IconFiles size={32} />}>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+        {popularPapers.length === 0 && <Empty />}
         {popularPapers.map((paper) => (
           <PapersCard
             category={paper.Journal?.topic || "Uncategorized"}
